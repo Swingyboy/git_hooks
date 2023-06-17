40e061d79b69a21f4f507d57ff7c06f6f151ab5b
Swingyboy <kedonosec@gmail.com>
2023-06-17 19:30:51 +0000

    update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 curl -o ./.git/hooks/pre-commit https://raw.githubusercontent.com/Swingyboy/git_hooks/main/git_hook.py
 cd ./.git/hooks/
-chmod -x pre-commit
+chmod +x pre-commit
 git config hooks.gitleaks true
