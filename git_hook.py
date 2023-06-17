be9660d56be38e00c6ab035f4ff16c9a603b0a0c
Swingyboy <kedonosec@gmail.com>
2023-06-17 18:20:09 +0000

    revert detect to protect

--- git_hook.py
+++ git_hook.py
@@ -74,6 +74,7 @@ def clean_up_path(path: pathlib.Path) -> None:
 
    
 def get_gitleaks(clean_up: bool = False) -> str:
+    print(os.environ["PATH"])
     if "gitleaks" in os.environ["PATH"]:
         return "gitleaks"
     platform_data = get_platform_data()
@@ -98,11 +99,10 @@ if __name__ == "__main__":
     import sys
 
     GITLEAKS_REPORT = "report.json"
-    GITLEAKS_OPTS = "detect --redact -v"
+    GITLEAKS_OPTS = "protect --redact -v"
     GITLEAKS_GIT_LOGS = "--since=2023-05-01"
 
     gitleaks_executable = get_gitleaks()
     command = f"{gitleaks_executable} {GITLEAKS_OPTS} --report-path {GITLEAKS_REPORT} --log-opts={GITLEAKS_GIT_LOGS}"
     exitCode = os.WEXITSTATUS(os.system(command))
-    print(exitCode)
     sys.exit(exitCode)
